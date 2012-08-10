2dcfbd1e1c1e0c0ffe04954ba3d633dd795ffc46
Jason Van Cleave <jvcleave@gmail.com>
2012-08-10 01:14:10 -0400

    adding server

---
+++ src/MongoServer.cpp
@@ -0,0 +1,20 @@
+/*
+ *  MongoServer.cpp
+ *  mongoTest
+ *
+ *  Created by jason van cleave on 8/10/12.
+ *  Copyright 2012 jasonvancleave.com. All rights reserved.
+ *
+ */
+
+#include "MongoServer.h"
+
+MongoServer::MongoServer()
+{
+
+}
+
+void MongoServer::setup()
+{
+	
+}
